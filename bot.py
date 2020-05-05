3fe5d1a4a534a9c402a2708b2f55ad4875cc84bf
AdenijiSamuel-coder <samuel.adeniji@student.aul.edu.ng>
2020-05-05 11:28:45 -0700

    new command ooin

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ import time
 bot_token = '1078127955:AAEpQvG8eDSMb7etCgwdaanNKfesPNkQyBY'
 
 bot = telebot.TeleBot(token=bot_token)
-@bot.message_handler(commands = ['start'])
+@bot.message_handler(commands = ['ooin'])
 def send_welcome(message):
-    bot.reply_to(message, 'welcome! how are you doing well')
+    bot.reply_to(message, 'You are doing well')
 
 bot.polling()
