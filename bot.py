b1db0f559e97bba20a3ed828ca757f9c00c9a8ca
AdenijiSamuel-coder <samuel.adeniji@student.aul.edu.ng>
2020-05-08 01:17:05 -0700

    the complete bot code

--- bot.py
+++ bot.py
@@ -4,8 +4,28 @@ import time
 bot_token = '1078127955:AAEpQvG8eDSMb7etCgwdaanNKfesPNkQyBY'
 
 bot = telebot.TeleBot(token=bot_token)
+
+def find_at(msg):
+    for text in msg:
+        if '@' in text:
+            return text
 @bot.message_handler(commands = ['ooin'])
 def send_welcome(message):
     bot.reply_to(message, 'You are doing well')
 
-bot.polling()
+@bot.message_handler(commands = ['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'To use this bot, send it a username')
+
+ @bot.message_handler(func=lambda msg: msg.text is not None and '@'  in msg.text)
+ def at_answer(message):
+     texts = message.text.split()
+     at_text = find_at(texts)
+
+     bot.reply_to(message, 'https://www.instagram.com/{}'.format(at_text[1:]))
+
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
